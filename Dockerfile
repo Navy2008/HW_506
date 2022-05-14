35bc36434f4eea8bcd28e0ad552abb91522d63df
Navy2008 <duartemanuel@hotmail.com>
2022-05-14 16:05:04 -0700

    push sh file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-
-
 FROM python:3.10.4-slim-bullseye
 RUN pip3 install flask flask-wtf email_validator requests flask-login flask-sqlalchemy
+# Copy files into container
 COPY app.py app.py
+COPY wiki.py wiki.py
+# Run the app
 CMD python app.py
--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def login():
     return render_template("login.html", form=form)
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", debug=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
+    #app.run(debug=True)
